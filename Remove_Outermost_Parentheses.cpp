bba666e476c2edd033205572a4b4844cbc857958
md-zeon <zeon.cse@gmail.com>
2025-12-25 23:59:28 +0600

    Enhance documentation with problem description, approach, and sample input/output

--- Remove_Outermost_Parentheses.cpp
+++ Remove_Outermost_Parentheses.cpp
@@ -1,5 +1,28 @@
+/************************************************************
+*  Problem: Remove Outermost Parentheses
+*  Link   : https://www.codechef.com/practice/course/strings/STRINGS/problems/REMOVEPAREN
+*  Author : Zeanur Rahaman Zeon
+*  Date   : 2025-12-25
+*  ----------------------------------------------
+
+*  Problem Description:
+*  You are given a valid parentheses string s. A valid parentheses string is composed of '(' and ')' that are properly balanced.
+*  A valid parentheses string is called primitive if it cannot be split into two smaller valid parentheses strings.
+*  Your task is to remove the outermost parentheses from every primitive part of s and print the final result.
+*
+*  Platform: CodeChef
+*  Status  : [Solved]
+************************************************************/
+
 #include <bits/stdc++.h>
 using namespace std;
+
+/************************************************************
+ * Approach: 
+ *
+ * Time Complexity:
+ * Space Complexity:
+ ************************************************************/
 
 string removeOuterParentheses(const string &s)
 {
@@ -48,6 +71,13 @@ string removeOuterParentheses(const string &s)
     return ans;
 }
 
+/************************************************************
+ * Approach:
+ *
+ * Time Complexity:
+ * Space Complexity:
+ ************************************************************/
+
 string removeOuterParenthesesOptimized(const string &s)
 {
     int depth = 0;
@@ -93,3 +123,18 @@ int main()
 
     return 0;
 }
+
+/************************************************************
+ * Sample Input:
+ * 4
+ * ((()))
+ * (()(()))
+ * ()()
+ * ((())())(()(()))
+ *
+ * Sample Output:
+ * (())
+ * ()(())
+ *
+ * (())()(())
+ ************************************************************/
